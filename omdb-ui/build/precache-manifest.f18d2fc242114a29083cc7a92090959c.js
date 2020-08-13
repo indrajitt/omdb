181bc34d9a58543f66cf03c27d17e3b88bc1cca9
indrajitt <f20181019@goa.bits-pilani.ac.in>
2020-08-13 10:04:14 +0530

    build

---
+++ omdb-ui/build/precache-manifest.f18d2fc242114a29083cc7a92090959c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed11fdb2b45d692076c5dde348bd8a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0654aeffeff2bc5a02d3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7472bd45a0446ae3e3d6",
+    "url": "/static/css/main.5743a943.chunk.css"
+  },
+  {
+    "revision": "0654aeffeff2bc5a02d3",
+    "url": "/static/js/2.863fd998.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.863fd998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7472bd45a0446ae3e3d6",
+    "url": "/static/js/main.8da4584a.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
